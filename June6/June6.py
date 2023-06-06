27f835210d0951bd0c2518b983f6e3f5fde65a89
bethjm <b.jeanne.mills@gmail.com>
2023-06-06 13:53:17 -0400

    changed file structure

---
+++ June6/June6.py
@@ -0,0 +1,8 @@
+# problem to solve: contains duplicate
+# given an integer array nums, reutn true if any value appears at least twice in the array, and return false if every element is distinct
+
+
+
+
+
+
