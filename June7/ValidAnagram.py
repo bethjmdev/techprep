3eeb14172c9b41ee39ce01a8ab9ae5aa16624a56
bethjm <b.jeanne.mills@gmail.com>
2023-06-07 09:23:22 -0400

    completed valid anagram

--- June7/ValidAnagram.py
+++ June7/ValidAnagram.py
@@ -1,1 +1,12 @@
 #continuing valid Anagram from yesterday
+
+#problem: valid anagram
+#given two string a and t, truen true if t is an anagram of s, and false otherwise
+
+#sorted order would be the a good way to check becuase they would become the exact same string, down side is time complexity
+
+
+
+class Solution:
+    def isAnagram (self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
