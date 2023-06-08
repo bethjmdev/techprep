8f8432877874afa5291e61963f878a1e2e6fba02
bethjm <b.jeanne.mills@gmail.com>
2023-06-08 11:25:46 -0400

    group anagrams neetcode

--- June8/GroupAnagrams.py
+++ June8/GroupAnagrams.py
@@ -4,3 +4,24 @@
 
 #youtube video to reference:
 #https://www.youtube.com/watch?v=vzdNOK2oB2E
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) #mapping character count to list of anagrams
+        #defaultdict makes the intial value of each a list
+
+        for s in strs:
+            count = [0] * 26 
+            # counts 26 0's
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+                #map the ascy value of each number
+                #counting each character we have
+
+            res[tuple(count)].append(s)
+            #lists in python cannot be keys so you must include tuple becuase they are non mutable
+            #group all anagrams in this particular count together
+
+            return res.values()
+
