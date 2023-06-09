00816a374d6b652a23849c130d75773edda05033
bethjm <b.jeanne.mills@gmail.com>
2023-06-09 13:45:28 -0400

    repracticing contains dup

---
+++ June9/ContainsDup.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set ()
+        #creating the hash set
+
+        for n in nums:
+        #n is the array of number we're looking at
+            if n in hashset:
+            #if n is in the hashset
+                return True
+            hashset.add(n)
+            #if it's not, add it to the hashset
