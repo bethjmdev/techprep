08816e22ff5e3c4f946e3260c636320d145d8415
bethjm <b.jeanne.mills@gmail.com>
2023-08-01 15:39:35 -0400

    adding if statement practice

---
+++ JavaScript_Practice/If_Statements.js
@@ -0,0 +1,21 @@
+// if statement is a basic for of decision making
+
+let age = 21;
+
+if (age > 18 && age < 64) {
+  console.log("you are an adult");
+} else if (age < 0) {
+  console.log("you havent been born yet");
+} else if (age >= 65) {
+  console.log("you are a senior citizen");
+} else {
+  console.log("you are not an adult");
+}
+
+let online = true;
+
+if (online) {
+  console.log("you are online");
+} else {
+  console.log("you are offline");
+}
