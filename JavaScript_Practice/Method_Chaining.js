87c960ef0f8553077eb3a32201db6053942cb37f
bethjm <b.jeanne.mills@gmail.com>
2023-08-01 15:33:41 -0400

    adding method chaining practice

---
+++ JavaScript_Practice/Method_Chaining.js
@@ -0,0 +1,8 @@
+// method chaining is calling one method after another in one continous line of code
+
+//charAt(0) tells you what character is at a given index
+
+let userName = "bro";
+
+let letter = userName.charAt(0).toUpperCase();
+console.log(letter);
