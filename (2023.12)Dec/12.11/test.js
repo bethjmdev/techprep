e661831559222d370261bd2a0e7c70e9ae8120d1
bethjm <b.jeanne.mills@gmail.com>
2023-12-12 15:07:57 -0500

    codewars

--- (2023.12)Dec/12.11/test.js
+++ (2023.12)Dec/12.11/test.js
@@ -1,19 +1,44 @@
-function removeSpaces(x) {
-  let newX = x.split(" ");
-  newX = newX.join("");
-  console.log("console", newX);
+function countCharacters(str) {
+  let alphabet = {
+    a: null,
+    b: null,
+    c: null,
+    d: null,
+    e: null,
+    f: null,
+    g: null,
+    h: null,
+    i: null,
+    j: null,
+    k: null,
+    l: null,
+    m: null,
+    n: null,
+    o: null,
+    p: null,
+    q: null,
+    r: null,
+    s: null,
+    t: null,
+    u: null,
+    v: null,
+    w: null,
+    x: null,
+    y: 1,
+    z: null,
+  };
 
-  return newX;
+  let newArray = [];
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] !== null) {
+      newArray.push(alphabet[i]);
+    }
+  }
+
+  console.log(newArray);
 }
 
-const testCases = [
-  "8 j 8   mBliB8g  imjB8B8  jl  B",
-  "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd",
-  "8aaaaa dddd r     ",
-];
-
-// Iterate through test cases and log the results
-testCases.forEach((test) => {
-  const result = removeSpaces(test);
-  console.log(`Input: "${test}" -> Output: "${result}"`);
-});
+// Call the function with a string
+countCharacters("aba"); // Output should be {'a': 2, 'b': 1}
+countCharacters(""); // Output should be {}
