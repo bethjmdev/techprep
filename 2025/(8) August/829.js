b808c7642d4f80fe4a0c2fd09bc9f0fb210ceb21
bethjm <b.jeanne.mills@gmail.com>
2025-08-29 12:57:20 -0400

    8.29 coding q's

--- 2025/(8) August/829.js
+++ 2025/(8) August/829.js
@@ -74,3 +74,104 @@ console.log(count)
 
 // ----------------------------
 
+//needed thiis one explained added comment for reference of understandinng
+
+
+// 3. Exam Submissions
+// Students submit assignments for multiple sections:
+
+// {A1: "sarah", A2: "tom", A3: "sarah", A4: "james"}
+
+
+// Detect which students submitted more than once.
+// Expected output: ["sarah"]
+
+
+
+const exam = {A1: "sarah", A2: "tom", A3: "sarah", A4: "james"}
+
+let submission = new Set()
+let dupe = new Set()
+
+
+//accessing the hashmap
+for (let student in exam) {
+
+	//renaming value to make it easier to type
+	let name = exam[student]
+
+	//asking if the Set has name
+	if (submission.has(name)){
+		//if submission has it then add it to dupe
+		dupe.add(name)
+	} else {
+	//else add it to submission
+	submission.add(name)
+	}
+}
+
+console.log(dupe)
+
+// ----------------------------
+
+// 1. Conference Attendees
+
+// You have a list of badge IDs mapped to names:
+
+
+// {101: "john", 102: "emma", 103: "mike", 104: "emma"}
+
+
+// Detect if any attendee registered more than once.
+
+
+// Expected output: ["emma"]
+
+const id = {101: "john", 102: "emma", 103: "mike", 104: "emma"};
+
+let names = new Set();
+let dupes = new Set();
+
+for (let person in id){
+	
+	if (names.has(id[person])){
+		dupes.add(id[person]);
+	} else {
+		names.add(id[person]);
+	}
+}
+
+console.log(dupes)
+
+// ----------------------------
+
+
+// 2. Parking Lot Cars
+// A parking lot assigns spots to license plates:
+
+// {1: "ABC123", 2: "XYZ789", 3: "LMN456", 4: "ABC123"}
+
+
+// Find which license plates appear more than once.
+// Expected output: ["ABC123"]
+
+
+
+const plates = {1: "ABC123", 2: "XYZ789", 3: "LMN456", 4: "ABC123"};
+
+
+let license = new Set();
+let duplicate = new Set();
+
+
+
+for (let car in plates) {
+	if (license.has(plates[car])){
+		duplicate.add(plates[car])
+	} else {
+		license.add(plates[car])
+	}
+}
+
+
+console.log(duplicate);
