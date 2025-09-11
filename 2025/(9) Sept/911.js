958c917faffa084593edcb807f9f5ce8268fd5e2
bethjm <b.jeanne.mills@gmail.com>
2025-09-11 12:52:37 -0400

    911 coding prep

--- 2025/(9) Sept/911.js
+++ 2025/(9) Sept/911.js
@@ -76,3 +76,87 @@ console.log(inventory)
 // }
 
 // --------------------------------
+
+
+// 👉 Goal: While left <= 40 → right stays still.
+
+// When left > 40 → right-- each round.
+
+
+
+// Question:
+// Given [5, 15, 25, 35, 45, 55, 65], print both values each round.
+
+
+// left: 5, right: 65 
+// left: 15, right: 65 
+// left: 25, right: 65 
+// left: 35, right: 65 
+// left: 45, right: 55 
+// left: 55, right: 45 
+// left: 65, right: 35
+
+// const s = [5, 15, 25, 35, 45, 55, 65];
+
+// let left = 0;
+// let right = s.length - 1;
+
+
+// while (left < s.length) {
+// 	console.log(s[left], s[right])
+
+// 	if (s[left] > 40) {
+// 		right--
+// 	}
+
+// 	left++
+
+
+// }
+
+
+// --------------------------------
+
+
+
+
+// Step 6: Anchor One Pointer, Move the Other
+
+// Goal: Keep the left pointer fixed, move the right pointer through the rest of the array, then move left one over and repeat.
+
+// Question 1:
+
+// Given [1, 2, 3, 4], print the pair of values each round.
+
+// Example Output:
+
+// left: 1, right: 2
+// left: 1, right: 3
+// left: 1, right: 4
+// left: 2, right: 3
+// left: 2, right: 4
+// left: 3, right: 4
+
+
+// const s = [1, 2, 3, 4];
+
+// let left = 0;
+// let right = left + 1;
+
+// while (left < s.length) {
+	
+	
+
+// 	while (right < s.length) {
+// 		 console.log(s[left], s[right])
+
+// 	 right++
+
+// 	}
+
+// 	left++
+// 	right = left + 1
+// }
+
+// --------------------------------
+
