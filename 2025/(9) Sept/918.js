a9c6e352a9da956287508f2cff3c2904d31222c5
bethjm <b.jeanne.mills@gmail.com>
2025-09-18 10:21:02 -0400

    918 coidng prep

---
+++ 2025/(9) Sept/918.js
@@ -0,0 +1,32 @@
+// Input: 
+// nums = [3, 7, 10, 15, 20, 25, 30], target = 15
+// Output: 3
+
+
+
+// const nums = [3, 7, 10, 15, 20, 25, 30]
+// const target = 15
+
+// let left = 0
+// let right = nums.length - 1;
+
+
+// let middle;
+
+// while (left <= right) {
+// 	middle = Math.floor((left + right) / 2)
+// 	midNum = nums[middle];
+
+// 	if (midNum === target) {
+// 		return middle
+// 	} else if (midNum < target) {
+// 		left = middle + 1
+// 	} else {
+// 		right = middle - 1
+// 	}
+// }
+
+// return -1
+
+// --------------------------------
+
