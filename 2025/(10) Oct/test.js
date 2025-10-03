d32ffe97eccc58f263ffd56a7fa3ea8b68813335
bethjm <b.jeanne.mills@gmail.com>
2025-10-03 13:00:00 -0400

    103 coding q

--- 2025/(10) Oct/test.js
+++ 2025/(10) Oct/test.js
@@ -1,76 +1,69 @@
-
-
 /**
- * @param {string} ransomNote
- * @param {string} magazine
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var canConstruct = function(ransomNote, magazine) {
+var removeDuplicates = function(nums) {
+    // implement here
 
-    // let rMap = {}
-    let mMap = {}
+    // console.log("nums", nums)
+    let left = 0;
+    let right = left + 1;
 
-    if (ransomNote.length === 0 ) {
-        return true
-    }
+    // let right = nums.length - 1;
 
-    for (let i = 0; i < magazine.length; i++) {
-        console.log("magI",magazine[i])
-        if (mMap[magazine[i]] === undefined) {
-            mMap[magazine[i]] = 1
+    while (right < nums.length) {
+        // console.log("while loop running")
+
+        // console.log("left",nums[left], "index",left,"right", nums[right],"index", right)
+
+        if (nums[left] === nums[right]) {
+            // console.log("## if loop running")
+            //remove the element
+            nums.splice(right, 1);
+            // console.log("nums after splice",nums)
+            //move right over one, leave left where it is
+            // right++
+            
         } else {
-            mMap[magazine[i]] += 1
+            // console.log("## ELSE RUNNING")
+            //move items over one
+            left+=1
+            right+=1
         }
+
+
     }
 
+// console.log("after",nums)
+    return (nums.length)
 
-    console.log(mMap)
-
-    for (let i = 0; i < ransomNote.length; i++) {
-        let letter = ransomNote[i]
-
-        console.log(letter)
-
-        console.log(mMap[letter])
-
-        if (mMap[letter] !== undefined && mMap[letter] > 0){
-            console.log("in IF block")
-            console.log(mMap[letter])
-            //if it's in there then subract it
-            mMap[letter] -= 1
-        } else {
-            return false
-        }
-    }
-
-    return true
 
 };
 
+// --- Test cases ---
+function test(nums, expectedNums) {
+    console.log("____________________________");
 
-// ------------------ TEST CASES ------------------
-function runTests() {
-    const tests = [
-        // { ransomNote: "a", magazine: "b", expected: false },
-        // { ransomNote: "aa", magazine: "ab", expected: false },
-        // { ransomNote: "aa", magazine: "aab", expected: true },
-
-        // // extra cases you can try
-        // { ransomNote: "", magazine: "anything", expected: true }, // empty ransom note
-        // { ransomNote: "abc", magazine: "cba", expected: true }, // exact letters
-        // { ransomNote: "abcd", magazine: "abc", expected: false }, // missing one
-        { ransomNote: "zzz", magazine: "zzzzzz", expected: true }, // plenty of repeats
-    ];
-
-    tests.forEach((test, i) => {
-        const result = canConstruct(test.ransomNote, test.magazine);
-        console.log(`Test ${i + 1}:`, 
-            "ransomNote =", test.ransomNote, 
-            "magazine =", test.magazine, 
-            "=> result:", result, 
-            "| expected:", test.expected
-        );
-    });
+    let copy = [...nums];
+    let k = removeDuplicates(nums);
+    // console.log("Input:", copy);
+    console.log("Returned k:",k);
+    console.log("Returned k:", k.length);
+    console.log("Expected k:", expectedNums.length);
+    console.log("Expected nums:", expectedNums);
+    console.log("____________________________");
 }
 
-runTests();
+test([1,1,2], [1,2]);
+test([0,0,1,1,1,2,2,3,3,4], [0,1,2,3,4]);
+test([], []);
+test([1], [1]);
+test([1,1,1,1], [1]);
+test([1,2,3], [1,2,3]);
+test([2,2,3,3,4,4,5,5,6], [2,3,4,5,6]);
+test([1,1,2,3,3,4,5,5,5,6], [1,2,3,4,5,6]);
+test([-3,-3,-2,-1,-1,0,0,1,2,2,3], [-3,-2,-1,0,1,2,3]);
+test([100,100,101,102,102,103,104,104,105], [100,101,102,103,104,105]);
+test([5,5,5,5,5,5,5,5,5], [5]);
+test([1,2,2,3,4,4,5,6,7,7,7,8], [1,2,3,4,5,6,7,8]);
+test([1,1,2,2,2,3,3,4,4,4,4,5], [1,2,3,4,5]);
